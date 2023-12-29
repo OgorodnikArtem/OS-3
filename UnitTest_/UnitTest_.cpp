092bc81ca10e0a11b3824a0cd6bdf9b0e5d65902
Artem Ogorodnik <Artemogorodnik2004@gmail.com>
2023-12-29 13:17:59 +0300

    New Unit tests

--- UnitTest_/UnitTest_.cpp
+++ UnitTest_/UnitTest_.cpp
@@ -6,6 +6,10 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+void SetK(size_t value) {
+    k = value;
+}
+
 namespace UnitTest
 {
 	TEST_CLASS(UnitTest)
@@ -32,6 +36,50 @@ namespace UnitTest
             Assert::AreEqual(3.0, testVector[2]);
             Assert::AreEqual(4.0, testVector[3]);
             Assert::AreEqual(5.0, testVector[4]);
+        }
+
+
+
+
+
+        TEST_METHOD_INITIALIZE(SetUp) {
+            InitializeCriticalSection(&cs);
+        }
+
+        TEST_METHOD_CLEANUP(TearDown) {
+            DeleteCriticalSection(&cs);
+        }
+
+        TEST_METHOD(Thread3Function_CalculatesCorrectly) {
+
+            result = 1;
+            std::vector<double> testData = { 2.0, 3.0, 4.0 };
+            k = 0;
+
+            DWORD threadId;
+            HANDLE hThread = CreateThread(NULL, 0, Thread3Function, &testData, 0, &threadId);
+            SetEvent(Event_2);
+
+            WaitForSingleObject(hThread, INFINITE);
+            CloseHandle(hThread);
+
+            Assert::AreEqual(2.0 * 3.0 * 4.0, (double) result);
+        }
+
+        TEST_METHOD(Thread3Function_SkipsElementsBeforeK) {
+            result = 1;
+
+            std::vector<double> testData = { 2.0, 3.0, 4.0 };
+            k = 1;
+
+            DWORD threadId;
+            HANDLE hThread = CreateThread(NULL, 0, Thread3Function, &testData, 0, &threadId);
+            SetEvent(Event_2);
+
+            WaitForSingleObject(hThread, INFINITE);
+            CloseHandle(hThread);
+
+            Assert::AreEqual(3.0 * 4.0, (double)result);
         }
 	};
 }
